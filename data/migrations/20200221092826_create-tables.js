a74cd97fd115c4f0c2de2f79c33a7d281bf318c3
Ammon Matthews <29493963+matthews7@users.noreply.github.com>
2020-02-21 09:46:58 -0700

    tables created

---
+++ data/migrations/20200221092826_create-tables.js
@@ -0,0 +1,60 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('project', tbl => {
+      tbl.increments();
+      tbl.string('project_name')
+      .notNullable();
+      tbl.string('project_description');
+      tbl.boolean('project_completed')
+      .notNullable()
+      .defaultTo(false);
+  })
+    .createTable('resource', tbl =>{
+        tbl.increments();
+        tbl.string('resource_name')
+            .notNullable()
+        tbl.string('resource_description');
+    })
+
+    .createTable('task', tbl => {
+        tbl.increments();
+        tbl.string('task_description')
+            .notNullable();
+        tbl.string('task_notes');
+        tbl.boolean('task_completed')
+            .notNullable()
+            .defaultTo(false);
+        tbl.integer('project_id')
+            .unsigned()
+            .notNullable()
+            .references('id')
+            .inTable('project')
+            .onUpdate('CASCADE')
+            .onDelete('CASCADE')
+    })
+
+    .createTable('project_resource', tbl => {
+        tbl.integer('project_id')
+            .unsigned()
+            .notNullable()
+            .references('id')
+            .inTable('project')
+            .onUpdate('CASCADE')
+            .onDelete('CASCADE')
+
+        tbl.integer('resource_id')
+            .unsigned()
+            .notNullable()
+            .references('id')
+            .inTable('resource')
+            .onUpdate('CASCADE')
+            .onDelete('CASCADE')
+    })
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('project')
+        .dropTableIfExists('resource')
+        .dropTableIfExists('task')
+        .dropTableIfExists('project_resource')
+};
